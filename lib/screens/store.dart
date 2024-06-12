5f89b55563691e58b2f66703a7f3e5415e62def3
Mrinmay Das <mrinmaydas2604@gmail.com>
2024-06-13 02:52:59 +0530

    done

--- lib/screens/store.dart
+++ lib/screens/store.dart
@@ -61,155 +61,167 @@ class StoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(top: 45.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  "Fit-Fit Store",
-                  style: TextStyle(
-                    fontSize: 21.0,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 149, 234, 244), // Light Blue
+              Color(0xFFF5F5F5), // Light Grey
+            ],
+            stops: [0.0, 0.7],
+          ),
+        ),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 45.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Fit-Fit Store",
+                    style: TextStyle(
+                      fontSize: 21.0,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CarouselSlider(
-            options: CarouselOptions(
-              height: 132.0,
-              initialPage: 0,
-              enableInfiniteScroll: true,
-              reverse: false,
-              autoPlay: true,
-              autoPlayInterval: const Duration(seconds: 2),
-              autoPlayAnimationDuration: const Duration(milliseconds: 800),
-              autoPlayCurve: Curves.fastOutSlowIn,
-              scrollDirection: Axis.horizontal,
-              enlargeCenterPage: true,
-              enlargeFactor: 0.2,
-              viewportFraction: 0.8,
+            const SizedBox(
+              height: 20,
             ),
-            items: [1, 2, 3].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  final List<String> phrase = [
-                    'Strength',
-                    'Fitness',
-                    'Health',
-                  ];
-                  return Container(
-                    width: MediaQuery.of(context).size.width,
-                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/carousel/car$i.png'),
-                        fit: BoxFit.cover,
-                        colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.28),
-                          BlendMode.darken,
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 132.0,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 2),
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                scrollDirection: Axis.horizontal,
+                enlargeCenterPage: true,
+                enlargeFactor: 0.2,
+                viewportFraction: 0.8,
+              ),
+              items: [1, 2, 3].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    final List<String> phrase = [
+                      'Strength',
+                      'Fitness',
+                      'Health',
+                    ];
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/carousel/car$i.png'),
+                          fit: BoxFit.cover,
+                          colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.28),
+                            BlendMode.darken,
+                          ),
                         ),
                       ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        phrase[i - 1],
-                        style: const TextStyle(
-                            fontSize: 25.0, color: Colors.white),
+                      child: Center(
+                        child: Text(
+                          phrase[i - 1],
+                          style: const TextStyle(fontSize: 25.0, color: Colors.white),
+                        ),
                       ),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(right: 220.0),
+              child: Text(
+                "Today's Best Offers",
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 18,
+            ),
+            Expanded(
+              child: GridView.builder(
+                padding: const EdgeInsets.all(16.0),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                  childAspectRatio: 0.75,
+                ),
+                itemCount: storeItems.length,
+                itemBuilder: (context, index) {
+                  final item = storeItems[index];
+                  return Card(
+                    elevation: 2.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          item.imageUrl,
+                          height: 140.0,
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          item.name,
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              FontAwesome5Solid.coins,
+                              color: Color.fromARGB(255, 231, 175, 7),
+                              size: 16,
+                            ),
+                            SizedBox(width: 5.0),
+                            Text(
+                              item.currentPrice,
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 5.0),
+                        Text(
+                          item.originalPrice,
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            decoration: TextDecoration.lineThrough,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ],
                     ),
                   );
                 },
-              );
-            }).toList(),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(right: 220.0),
-            child: Text(
-              "Today's Best Offers",
-              style: TextStyle(
-                fontSize: 18.0,
               ),
             ),
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(16.0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 10.0,
-                childAspectRatio: 0.75,
-              ),
-              itemCount: storeItems.length,
-              itemBuilder: (context, index) {
-                final item = storeItems[index];
-                return Card(
-                  elevation: 2.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        item.imageUrl,
-                        height: 140.0,
-                      ),
-                      const SizedBox(height: 10.0),
-                      Text(
-                        item.name,
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                        ),
-                      ),
-                      const SizedBox(height: 5.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            FontAwesome5Solid.coins,
-                            color: Color.fromARGB(255, 231, 175, 7),
-                            size: 16,
-                          ),
-                          const SizedBox(width: 5.0),
-                          Text(
-                            item.currentPrice,
-                            style: const TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.red,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 5.0),
-                      Text(
-                        item.originalPrice,
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          decoration: TextDecoration.lineThrough,
-                          color: Colors.grey[600],
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
