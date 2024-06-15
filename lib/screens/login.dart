c077ed9f285996892391104e119089cd92825020
Mrinmay Das <mrinmaydas2604@gmail.com>
2024-06-15 18:20:57 +0530

    bug fix

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:watch/screens/splash_screen.dart';
+import 'package:watch/screens/tabs.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -19,6 +20,11 @@ class LoginScreen extends StatelessWidget {
                 onPressed: () async {
                   var SharedPref = await SharedPreferences.getInstance();
                   SharedPref.setBool(SplashScreenState.KEYLOGIN, true);
+                  await Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const TabsScreen(),
+                      ));
                 },
                 child: const Text("login"))
           ],
