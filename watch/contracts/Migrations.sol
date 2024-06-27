fad0e59620ccf4f432cd25a3d6508d73c7d182a6
techmonk000 <swarnavomukherjee03@gmail.com>
2024-06-28 01:44:42 +0530

    rest-change

--- watch/contracts/Migrations.sol
+++ watch/contracts/Migrations.sol
@@ -1,11 +1,10 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
-  constructor()  {
+  constructor() public {
     owner = msg.sender;
   }
 
